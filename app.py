0ba626c2dd19eadea7f54543a0d25e296c867a33
Nathan Fraysse <foxou06@gmail.com>
2025-12-13 16:53:22 +0100

    #1 et #4 modif de la fonction pour quelle le renvoie sur laffichage

--- app.py
+++ app.py
@@ -3,16 +3,14 @@ from tkinter import ttk
 import random
 
 #fonction pour générer le mot de passe :
-length=int(input("Entrer la longueur du mot de passe : "))
-def creation_mdp(length):
+def creation_mdp():
+    length=int(champ_saisi.get())
     char_possible="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+=-|"
     password=""
     for i in range(length):
         rand_index=random.randint(0, len(char_possible) - 1)
         password+=char_possible[rand_index]
-    return password
-
-print(creation_mdp(length))
+    return affichage.config(text=password)
 
 mdp=Tk()
 mdp.title("Générateur de mot de passe")
